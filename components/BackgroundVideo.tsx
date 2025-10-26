116dfe515b39315b1b340011c3c6382bf808ebab
Davis <davis.ernst@outlook.com>
2025-10-25 23:14:53 -0600

    Add background video component

---
+++ components/BackgroundVideo.tsx
@@ -0,0 +1,6 @@
+export default function BackgroundVideo() {
+  return (
+    <video autoPlay src="/videos/combined.mp4" loop muted disablePictureInPicture playsInline className="object-cover w-full h-screen">
+    </video>
+  )
+}
